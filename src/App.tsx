394de559dbe815fb8a1cb2cb4efb79cee412ea1a
Ark Deliev <arkadiy.deliev@soprasteria.com>
2025-07-21 16:27:03 +0200

    refactor: remove unused SlateDemo route

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,6 @@ import Chat from "./pages/Chat";
 import Settings from "./pages/Settings";
 import NotFound from "./pages/NotFound";
 import EditorPage from "./components/Editor/EditorPage";
-import SlateDemo from "./pages/SlateDemo";
-// import SlateDemoTest from "./pages/SlateDemoTest";
 
 const queryClient = new QueryClient();
 
@@ -22,7 +20,6 @@ const App = () => (
           <Route path="/chat" element={<Chat />} />
           <Route path="/settings" element={<Settings />} />
           <Route path="/editor" element={<EditorPage />} />
-          <Route path="/slate-demo" element={<SlateDemo />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
